f76db9fda3d4afb4aa6f335b88eafaef8c76839d
ogasege <oluwasegunadedigba@gmail.com>
2023-07-23 19:53:16 +0100

    print slot machine function defined

--- main.py
+++ main.py
@@ -38,7 +38,14 @@ def get_slot_machine_spin(rows, cols, symbols):
         
     return columns
     
-    
+def print_slot_machine(columns):   
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+        
     
     
 
